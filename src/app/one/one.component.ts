861b432bf6ccb3f7a788002ea3d59de6fbca05f6
Jayvant Pawar <pawarjayvant16@gmail.com>
2023-07-03 20:10:46 +0530

    first commit

---
+++ src/app/one/one.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector : 'ea-one',
+    template : '<h1>One Component Works</h1>',
+    styles : ['h1{color:red}'],
+})
+
+export class OneComponent{}
